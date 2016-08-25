42a52d978f22f72d0e347495c69ceec69226c50f
kiwamu <kiwamu@00639-1>
2016-08-25 10:02:02 +0900

    プロジェクト ファイルを追加します。

---
+++ KakeiboApp/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(KakeiboApp.Startup))]
+namespace KakeiboApp
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
